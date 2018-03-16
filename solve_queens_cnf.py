7f723ad5c98ca1df2e0bf21ee44f84b65729f8cc
golikov-nik <golikovnik@gmail.com>
2018-03-16 18:52:43 +0300

    add requirements.txt, clean up the code

--- solve_queens_cnf.py
+++ solve_queens_cnf.py
@@ -1,51 +1,46 @@
 import pycosat
 import itertools
 import argparse
+import numpy as np
+
+
+def encode(n, x, y):
+	return 1 + n * y + x
+
+
+def generate_diagonals(n, x, y, dx, dy):
+	while 0 <= x < n and 0 <= y < n:
+		yield -encode(n, x, y)
+		x += dx
+		y += dy
+
+
+def all_pairs(iterable):
+	return itertools.combinations(iterable, 2)
+
+
+def only_one_queen(cells):
+	#at least one
+	yield cells
+
+	#no more than one
+	yield from all_pairs((-x for x in cells))
 
 
 def generate_cnf(n):
-	clauses = []
-	#only one true on the rows
 	for row in range(n):
-		clauses.append(range(1 + n * row, 1 + n * (row + 1)))
-		clauses.extend(itertools.combinations(range(-1 - n * row, -1 - (row + 1) * n, -1), 2))
-	#only one true om the columns
+		yield from only_one_queen(range(encode(n, 0, row), encode(n, 0, row + 1)))
 	for column in range(n):
-		clauses.append(range(1 + column, 1 + column + n * n, n))
-		clauses.extend(itertools.combinations(range(-1 - column, -1 - column - n * n, -n), 2))
-	#maximum one true on looking left diagonals
+		yield from only_one_queen(range(encode(n, column, 0), encode(n, column + n * n, 0), n))
 	for i in range(n):
-		diag = []
-		x, y = i, n - 1
-		while x >= 0 and y >= 0:
-			diag.append(-1 -(y * n + x))
-			x -= 1
-			y -= 1
-		clauses.extend(itertools.combinations(diag, 2))
-		diag.clear()
-		x, y = n - 1, i
-		while x >= 0 and y >= 0:
-			diag.append(-1 -(y * n + x))
-			x -= 1
-			y -= 1
-		clauses.extend(itertools.combinations(diag, 2))
-	#maximum one true on looking right diagonals
-	for i in range(n):
-		diag = []
-		x, y = 0, i
-		while 0 <= x < n and 0 <= y < n:
-			diag.append(-1 -(y * n + x))
-			x += 1
-			y -= 1
-		clauses.extend(itertools.combinations(diag, 2))
-		diag.clear()
-		x, y = i, n - 1
-		while 0 <= x < n and 0 <= y < n:
-			diag.append(-1 -(y * n + x))
-			x += 1
-			y -= 1
-		clauses.extend(itertools.combinations(diag, 2))
-	return clauses
+		#looking left
+		yield from all_pairs(generate_diagonals(n, i, n - 1, -1, -1))
+		yield from all_pairs(generate_diagonals(n, n - 1, i, -1, -1))
+		
+		#looking right
+		yield from all_pairs(generate_diagonals(n, 0, i, 1, -1))
+		yield from all_pairs(generate_diagonals(n, i, n - 1, 1, -1))
+
 
 
 def main():
@@ -53,14 +48,14 @@ def main():
 	parser.add_argument('--count', help='determine the number of the solutions', action='store_true')
 	parser.add_argument('n', type=int, help='board size')
 	args = parser.parse_args()
-	cnf = generate_cnf(args.n)
+	cnf = list(generate_cnf(args.n))
 	print("Number of clauses in the CNF formula:", len(cnf))
 	result = pycosat.solve(cnf)
 	if result == 'UNSAT':
 		print("No solution")
 	else:
-		for row in range(args.n):
-			print(*('.' if x < 0 else 'Q' for x in result[row * args.n : row * args.n + args.n]), sep='')
+		for row in np.array_split(result, args.n):
+			print(*('_' if x < 0 else 'Q' for x in row), sep='')
 		if args.count:
 			print('Total solutions:', len(list(pycosat.itersolve(cnf))))
 
